1f8f0a1d8f5a98ccc5a95f7ff4c58e6e96817117
Gabriel Valencia <gabrielvalencia@Gabriels-MacBook-Pro-3.local>
2022-08-22 18:42:13 -0500

    Part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Cape_Royal_Sunset.jpg/1920px-Cape_Royal_Sunset.jpg';
+  image3 = 'https://www.tripsavvy.com/thmb/YAGkm1UoHJKSAbzn8KANU3GYGQM=/900x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv()/mount-rainier-national-park-945381454-fd1957f1232d499f986ca4e03e257431.jpg';
 
   constructor() { }
 
